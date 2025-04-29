0ad0eabd54e59912b6cb433e9043b624ed9afbeb
chenyuan <cy476571@gmail.com>
2025-04-29 17:07:47 +0800

    bugfix

--- install_deps.sh
+++ install_deps.sh
@@ -4,8 +4,9 @@
 
 # Test if numpy version issues exists.
 python -c 'from transformers.generation.utils import *'
+python -c 'import transformers.models.clip.modeling_clip'
 
-pip uninstall numpy
+pip uninstall numpy -y
 
 # Intall gpu sglang deps.
 pip install -e '.[gpu]' --find-links https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/
--- test_olmocr.sh
+++ test_olmocr.sh
@@ -2,4 +2,7 @@
 
 set -ex
 
+python -c 'from transformers.generation.utils import *'
+python -c 'import transformers.models.clip.modeling_clip'
+
 python -m olmocr.pipeline ./localworkspace --pdfs tests/gnarly_pdfs/horribleocr.pdf
