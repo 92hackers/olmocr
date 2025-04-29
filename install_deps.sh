3c3dca9fe6fec998458d409005674db4812c9cbc
chenyuan <cy476571@gmail.com>
2025-04-29 18:45:14 +0800

    bugfix

--- install_deps.sh
+++ install_deps.sh
@@ -6,7 +6,10 @@
 python -c 'from transformers.generation.utils import *'
 python -c 'import transformers.models.clip.modeling_clip'
 
-pip uninstall numpy -y
+pip uninstall numpy scipy -y
 
 # Intall gpu sglang deps.
 pip install -e '.[gpu]' --find-links https://flashinfer.ai/whl/cu124/torch2.4/flashinfer/
+
+pip install scipy==1.15.2
+pip install numpy==1.26.4
