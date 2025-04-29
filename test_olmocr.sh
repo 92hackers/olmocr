00a37d817b13112f472b806b1f7ed17fe53ec487
chenyuan <cy476571@gmail.com>
2025-04-29 19:53:56 +0800

    fix

--- test_olmocr.sh
+++ test_olmocr.sh
@@ -2,7 +2,11 @@
 
 set -ex
 
+# Make sure the dependencies are correctly installed
 bash ./check_deps.sh
+
+# Kill any existing python processes
+ps aux | grep python | grep -v grep | awk '{print $2}' | xargs kill -9
 
 pdf_files=./tests/gnarly_pdfs/horribleocr.pdf
 
